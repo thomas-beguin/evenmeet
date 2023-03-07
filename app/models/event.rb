f4600eaf1df5e3c4b70bf867cec1c2b54152578f
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-03-07 16:09:23 +0100

    geocoding basic configuration

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   has_many_attached :photos
 
   validates :end_date, comparison: { greater_than: :start_date }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
