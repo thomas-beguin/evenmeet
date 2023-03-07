aa4980ca46b5c5d27ea5972725ad15f9a9e9c780
marie langlois <marie.langlois2@gmail.com>
2023-03-07 18:24:47 +0100

    j'ai perdu la navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-
 
   devise_for :users
-
-  get "users/:id", to "user_events#show",
-  post "users/:id", to "user_events#show",
-
-  get "users/:id", to "challenges#show"
 
 end
