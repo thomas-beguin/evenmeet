26b3bb7f3b12f4efb836388ec50f7bab6662badd
Louis Sebag <sebag.louis@gmail.com>
2023-03-07 16:24:55 +0100

    push

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :rewards, dependent: :destroy
 
-  # validates :end_date, comparison: { greater_than: :start_date }
+  validates :end_date, comparison: { greater_than: :start_date }
 end
