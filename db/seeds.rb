3855ad127407cc3f7c561e7004467d88e56951a6
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-03-08 10:52:19 +0100

    Exo 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,9 +31,19 @@ solidays = Event.new(title: "Solidays",
                      description: "Solidays est un festival de musique organisé par l'association Solidarité sida.
                                    Depuis 1999, il rassemble, sur les pelouses de l'hippodrome de Longchamp, à Paris,
                                    des artistes, conférenciers, militants et festivaliers.",
-                     start_date: Date.today,
-                     end_date: Date.today + 30)
+                     start_date: DateTime.now + 30,
+                     end_date: DateTime.now + 32)
 solidays.save!
+
+delta_festival = Event.new(title: "Delta Festival",
+  category: "Festival de musique",
+  tags: "",
+  address: "50 Rue de l'Ourcq, 75019 Paris",
+  description: "Le Delta Festival est un festival bidon issu de l'imagination de Louis Sebag.",
+  start_date: DateTime.now + 45,
+  end_date: DateTime.now + 47)
+delta_festival.save!
+
 puts "Events created : #{Event.count}"
 puts "---------------------------------------"
 puts "Creating Participations"
