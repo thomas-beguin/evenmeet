ef0eb5e28f400d0fce6302b799334968e5bf4a3b
Louis Sebag <sebag.louis@gmail.com>
2023-03-09 18:12:37 +0100

    geocoding

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @events = Event.all
+      if params[:query].present?
+        params[:geo] = ""
+        p "kkkko"
+        sql_query = "title ILIKE :query OR description ILIKE :query"
+        @events = Event.where(sql_query, query: "%#{params[:query]}%")
+      elsif params[:geo].present?
+        params[:query] = ""
+        @events = Event.near(params[:geo])
 
-    if params[:query].present?
-      @events = Event.where(title: params[:query])
-    else
-      @events = Event.all
-    end
+      end
     # @events = Event.all
 
     # @markers = @events.geocoded.map do |event|
