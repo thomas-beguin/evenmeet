10726d7a8f93ed0a626a0de29f771e10f7f07ae7
Thomas Picamoles <thomas.picamoles@gmail.com>
2023-03-09 10:08:26 +0100

    seed version 1

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,7 +2,7 @@ class Relationship < ApplicationRecord
   # Permet de faire : relation.participation_sender = participation
   # Sinon, on doit passer par l'id
   belongs_to :sender,    class_name: "Participation"
-  
+
   # Permet de faire : relation.participation_receiver = participation
   # Sinon, on doit passer par l'id
   belongs_to :receiver, class_name: "Participation"
--- app/models/reward_choice.rb
+++ app/models/reward_choice.rb
@@ -1,5 +1,5 @@
 class RewardChoice < ApplicationRecord
   belongs_to :participation
   belongs_to :reward
-  enum :status, { pending: 0, accepted: 1, refused: 2 }
+  enum :status, { non_accepted: 0, accepted: 1 }
 end
