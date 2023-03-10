73937a558c10365dee8d0a7e38822b4c38a51ca8
thomas beguin <thom.beguin@gmail.com>
2023-03-10 23:00:12 +0100

    manque lien des channels messages

--- app/javascript/controllers/toggle_element_controller.js
+++ app/javascript/controllers/toggle_element_controller.js
@@ -2,11 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-element"
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["modal", "menuactive"]
   connect() {
+    console.log("coucou")
   }
 
   toggle() {
     this.modalTarget.classList.toggle("active");
+  }
+
+  menuactive() {
+    console.log("coucou act")
+    this.modalTarget.classList.toggle("active");
+    this.menuactiveTarget.classList.toggle("menu-active");
+    this.menuactiveTarget.classList.toggle("fa-bars");
+    this.menuactiveTarget.classList.toggle("fa");
+    this.menuactiveTarget.classList.toggle("fa-solid");
+    this.menuactiveTarget.classList.toggle("fa-chevron-left");
   }
 }
